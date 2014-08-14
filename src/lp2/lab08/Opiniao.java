c73d9073f4bf9f756637583a9b41cf6dd86ea406
113210410 FLABIO FAGUNDES ARAUJO DA MATTA <flabiofam@lcc2-24.lcc.ufcg.edu.br>
2014-08-14 11:21:09 -0300

    Adicionnado mudancas

---
+++ src/lp2/lab08/Opiniao.java
@@ -0,0 +1,29 @@
+package lp2.lab08;
+
+public class Opiniao {
+	
+	private int nota;
+	private String comentario;
+	private String data;
+	public int getNota() {
+		return nota;
+	}
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	
+
+}
--- src/lp2/lab08/Produto.java
+++ src/lp2/lab08/Produto.java
@@ -1,5 +1,20 @@
 package lp2.lab08;
 
+import java.util.ArrayList;
+
 public class Produto {
+	private String nome;
+	private ArrayList<Opiniao> opinioes;
+	private Estrategia estrategia;
+	private double notaNaMosca;
+	
+	public void visualizar(Estrategia estrategia){
+		for (Opiniao opiniao : opinioes) {
+			
+		}
+		
+		
+		
+	}
 
 }
